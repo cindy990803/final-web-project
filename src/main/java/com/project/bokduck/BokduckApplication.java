492a533c3a5eb71835ffcdb34df49126d6faaccc
mingkyy <alsrus1227@naver.com>
2021-09-11 13:14:06 +0900

    Initial commit

---
+++ src/main/java/com/project/bokduck/BokduckApplication.java
@@ -0,0 +1,13 @@
+package com.project.bokduck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BokduckApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BokduckApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/bokduck/BokduckApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.bokduck;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BokduckApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
